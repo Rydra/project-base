742f2d7a3f1a37a8f0e2e1d0795b0a1efb9a2977
David Jiménez <davigetto@gmail.com>
2023-01-17 20:21:16 +0100

    test: add one more test

--- tests/unit/test_sample.py
+++ tests/unit/test_sample.py
@@ -1,5 +1,7 @@
 from hamcrest import *
 
+from contexts.sample.core.domain.domain import Sample
+
 
 def test_sum():
     assert 1 == 1
@@ -7,3 +9,8 @@ def test_sum():
 
 async def test_sum_async(anyio_backend):
     assert_that(1, is_(1))
+
+
+def test_create_aggregate():
+    sample = Sample.new()
+    assert_that(sample, is_not(none()))
